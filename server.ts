9c34baa6b142091c485c325b6f733c2d16e42dcf
Magdy Abas <132621536+magdy-abas@users.noreply.github.com>
2025-10-02 22:31:16 +0300

    refactor:server.ts && app.config.server

--- server.ts
+++ server.ts
@@ -17,6 +17,8 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
+  // Example Express Rest API endpoints
+  // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get(
     '*.*',
@@ -25,32 +27,9 @@ export function app(): express.Express {
     })
   );
 
-  // Routes that should only use CSR (fallback to index.html)
-  const csrOnlyRoutes = [
-    '/courses',
-    '/booking',
-    '/instructor-register',
-    '/courses/category',
-    '/courses/doctor',
-    '/course-details',
-    '/categories',
-    '/instructors',
-    '/instructor-profile',
-    '/login',
-    '/signup',
-    '/forgotpass',
-    '/profile',
-    '/course-quiz',
-  ];
-
-  // All other routes
+  // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
-
-    if (csrOnlyRoutes.some((r) => req.url.startsWith(r))) {
-      res.sendFile(join(browserDistFolder, 'index.html'));
-      return;
-    }
 
     commonEngine
       .render({
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,24 +1,9 @@
-import {
-  mergeApplicationConfig,
-  ApplicationConfig,
-  InjectionToken,
-} from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 
-export const HOSTNAME = new InjectionToken<string>('HOSTNAME');
-
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    {
-      provide: HOSTNAME,
-      useFactory: () => {
-        const host = (global as any)?.process?.env?.HOSTNAME_OVERRIDE;
-        return host || 'localhost';
-      },
-    },
-  ],
+  providers: [provideServerRendering()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
